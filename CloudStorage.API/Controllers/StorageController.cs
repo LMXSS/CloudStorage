ecf629acf57f0b0cc3872a1ece5f3ed88354b517
Gabriel Lamas <glamas.2003@gmail.com>
2024-03-16 17:33:27 -0300

    add .APi project

---
+++ CloudStorage.API/Controllers/StorageController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CloudStorage.Application.UseCases.Users.UploadProfilePhoto;
+
+namespace CloudStorage.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StorageController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult UploadImage([FromServices] IUploadProfilePhotoUseCase useCase,IFormFile file)
+    {
+
+
+        useCase.Execute(file);
+        return Created();
+    }
+}
